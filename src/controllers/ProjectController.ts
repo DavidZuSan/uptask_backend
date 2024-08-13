766941df30095ff09f238b8d3abf38077f2403d6
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-08-13 13:48:38 +0200

    Creando Rutas

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from 'express';
+import Project from '../models/Project';
+
+export class ProjectController {
+    // Implement your project controller methods here
+    static createProject = async (req: Request, res:  Response) => {
+        const project = new Project(req.body);
+        try {
+            await project.save(req.body);
+            res.send('Proyecto Creado Correctamente');
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    static getAllProjects = async (req: Request, res:  Response) => {
+        res.send('Todos los Proyectos');
+    };
+}
